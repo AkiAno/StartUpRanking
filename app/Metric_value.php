dc2a775bd89701d87f91a1944a663caae71befbe
Aastha Sharma <aasthasharma092@gmail.com>
2018-11-04 15:25:56 +0100

    added model files with their relationships

---
+++ app/Metric_value.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Metric_value extends Model
+{
+    protected $table = 'metric_values';
+
+    public function account() 
+    {
+        return $this->belongsTo('App\Account','account_id','id');
+    }
+
+    public function metric_description() 
+    {
+        return $this->belongsTo('App\Metric_description','metric_description_id','id');
+    }
+}

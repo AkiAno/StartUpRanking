fe57fe69210ea2041996450c0841ee58b727d7cb
kybernetic <kybernetic@tuta.io>
2018-11-21 11:46:29 +0100

    fixed pages controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -22,14 +22,11 @@ class PageController extends Controller
         return view('pages/news');
     }
 
-<<<<<<< HEAD
-=======
-
     public function show($company){
         $company = Company::find($company);
         $networks = Network::all();
         // return $company;
         return view('pages/show', compact('company','networks'));
     }
->>>>>>> 08dfdec5e9c79793dd5e6397153be72d37d373b8
+
 }
